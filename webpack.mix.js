61d93c9470e04cf233d0c9f43d320c07b77ab41b
Sohail Ahmed <sohail.comsians@gmail.com>
2021-12-07 23:23:12 +0500

    vuejs fiexe

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/auth.js', 'static/dashboard/assets/js')
-    .js('resources/js/portal.js', 'static/dashboard/assets/js');
+mix.js('resources/js/auth.js', 'authentication/static/authentication/assets/js')
+    .js('resources/js/portal.js', 'dashboard/static/dashboard/assets/js');
